4312fdef17b50e5809830a2ac499b4727e16edce
CzNd <907772670@qq.com>
2025-10-15 15:40:36 +0800

    新增查询下级部门和本部门用户api，用于发布任务（暂不包含角色验证）

--- backend/src/main/java/com/nullworking/repository/DepartmentRepository.java
+++ backend/src/main/java/com/nullworking/repository/DepartmentRepository.java
@@ -1,5 +1,7 @@
 package com.nullworking.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,4 +9,6 @@ import com.nullworking.model.Department;
 
 @Repository
 public interface DepartmentRepository extends JpaRepository<Department, Integer> {
+    List<Department> findByParentDepartment_departmentId(Integer parentDepartmentId);
+    List<Department> findByParentDepartment_departmentIdIsNull();
 }
--- backend/src/main/java/com/nullworking/repository/UserRepository.java
+++ backend/src/main/java/com/nullworking/repository/UserRepository.java
@@ -1,5 +1,8 @@
 package com.nullworking.repository;
 
+import java.util.List;
+import java.util.Set;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,4 +11,5 @@ import com.nullworking.model.User;
 @Repository
 public interface UserRepository extends JpaRepository<User, Integer> {
 	User findByUserName(String userName);
+    List<User> findByDepartmentDepartmentIdIn(Set<Integer> departmentIds);
 }
